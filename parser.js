31d6c24bd39eb1be07a2e352716150aa937a6bfc
Kuldeep Panchal <pkuldeepr@gmail.com>
2021-05-02 19:17:38 +0530

    node and port

--- parser.js
+++ parser.js
@@ -77,7 +77,7 @@ app.post('/send', (req, res) => {
     async function sendEmail(subject, body){
         let transporter = nodemailer.createTransport({
             host: 'smtp.gmail.com',
-            port: PORT,
+            port: port,
             secure: false,
             auth: {
                 user: "amazonpricetracker0@gmail.com",
